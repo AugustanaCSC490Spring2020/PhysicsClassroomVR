f65b35e3a9cef5c53f52bde4e68ee2e69bd26439
hbader <hunterbader16@augustana.edu>
2020-04-14 10:52:21 -0500

    Added Test

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour //I am a donkey.
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
