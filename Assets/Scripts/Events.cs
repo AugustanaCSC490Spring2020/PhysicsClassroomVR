fab7111574acc35e8d7d4a1f0dfa597c2a98e455
mbenson20 <matthewbenson17@augustana.edu>
2020-04-20 19:45:15 -0500

    Can change size/mass of ball for 2nd Law table

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public void OnIncreaseMassButtonPress(GameObject obj)
+    {
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        obj.transform.localScale += new Vector3(.1f,.1f,.1f);
+        rb.mass = rb.mass * 1.5f;
+    }
+
+    public void OnDecreaseMassButtonPress(GameObject obj)
+    {
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        obj.transform.localScale -= new Vector3(.1f, .1f, .1f);
+        rb.mass = rb.mass * .5f;
+    }
+
+    public void OnFreeBodyButtonPress()
+    {
+
+    }
+}
