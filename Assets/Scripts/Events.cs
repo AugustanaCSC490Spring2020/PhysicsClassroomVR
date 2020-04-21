62bbe3a85d52cc3f96fe415878d2b31a5dea4789
mbenson20 <matthewbenson17@augustana.edu>
2020-04-20 21:12:57 -0500

    Added Ball Respawn and some Description. Happy Arrow for force diagram

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Events : MonoBehaviour
 {
+
+    public TextMeshProUGUI massText;
+
     public void OnIncreaseMassButtonPress(GameObject obj)
     {
+        
         Rigidbody rb = obj.GetComponent<Rigidbody>();
         obj.transform.localScale += new Vector3(.1f,.1f,.1f);
-        rb.mass = rb.mass * 1.5f;
+        rb.mass = rb.mass * 2f;
+        massText.SetText("Mass: {0}kg", rb.mass);
     }
 
     public void OnDecreaseMassButtonPress(GameObject obj)
@@ -16,6 +22,7 @@ public class Events : MonoBehaviour
         Rigidbody rb = obj.GetComponent<Rigidbody>();
         obj.transform.localScale -= new Vector3(.1f, .1f, .1f);
         rb.mass = rb.mass * .5f;
+        massText.SetText("Mass: {0}kg", rb.mass);
     }
 
     public void OnFreeBodyButtonPress()
