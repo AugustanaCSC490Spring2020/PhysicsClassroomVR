483017c5884b39f80cd45c8bcf80a6197ed34540
hbader <hunterbader16@augustana.edu>
2020-04-27 21:08:25 -0500

    Menu Button Functionality as Well as Chapter Panel Added

---
+++ Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEvents : MonoBehaviour
+{
+    public void ChapterClick()
+    {
+            SceneManager.LoadScene(this.name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ChapterSelectShow()
+    {
+        GameObject panel = new GameObject();
+        panel = GameObject.Find("ChapterSelectPanel");
+        panel.SetActive(!panel.activeSelf);
+
+    }
+
+    public void MenuOptionsShow()
+    {
+        GameObject panel = new GameObject();
+        panel = GameObject.Find("OptionsPanel");
+        panel.SetActive(!panel.activeSelf);
+    }
+}
