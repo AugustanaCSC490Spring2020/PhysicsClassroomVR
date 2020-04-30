b13f2de852b13c46f39bb8dc89450cd2c40bef4d
mbenson20 <matthewbenson17@augustana.edu>
2020-04-30 11:44:43 -0500

    Created Circuits Scene
    need to figure out how to draw "wires" on the gorund so we can connect the different components. I have some pointer/raycast stuff in the scene already

---
+++ Assets/Scripts/PhysicsPointer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsPointer : MonoBehaviour
+{
+    public float defaultLength = 3.0f;
+
+    private LineRenderer lineRenderer = null;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        UpdateLength();
+    }
+
+    private void UpdateLength()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, CalculateEnd());
+    }
+
+    private Vector3 CalculateEnd()
+    {
+        RaycastHit hit = CreateForwardRaycast();
+        Vector3 endPosition = DefaultEnd(defaultLength);
+
+        if (hit.collider)
+            endPosition = hit.point;
+
+        return endPosition;
+    }
+    
+    private RaycastHit CreateForwardRaycast()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        Physics.Raycast(ray, out hit, defaultLength);
+        return hit;
+    }
+
+    private Vector3 DefaultEnd(float length)
+    {
+        return transform.position + (transform.forward * length);
+    }
+
+
+}
