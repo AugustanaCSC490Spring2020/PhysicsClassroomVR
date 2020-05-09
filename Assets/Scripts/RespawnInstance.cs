8fa0f788be63046ed9aeab8f5f104c526bee28fc
mbenson20 <matthewbenson17@augustana.edu>
2020-05-09 00:00:57 -0500

    Got most of the circuits scene set up, need to get the respawn for each block working next

---
+++ Assets/Scripts/RespawnInstance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnInstance : MonoBehaviour
+{
+    public GameObject obj;
+    private Vector3 location;
+    public Vector3 limit;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        location = transform.position;
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void LateUpdate()
+    {
+
+        if (Mathf.Abs(transform.localPosition.x) > limit.x ||
+            (transform.localPosition.y < limit.y) ||
+            Mathf.Abs(transform.localPosition.z) > limit.z)
+        {
+            Instantiate<GameObject>(obj);
+            obj.transform.position = location;
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+        //Debug.Log(location.x + limit.x);
+    }
+}
